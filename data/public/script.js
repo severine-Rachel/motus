f73b8af5add27088dc66fbc63ae946be38b57bcc
severine-Rachel <severine.roux4@gmail.com>
2024-03-01 13:12:32 +0100

    check right word

--- data/public/script.js
+++ data/public/script.js
@@ -1,3 +1,4 @@
+/*
 function checkWord(userword, word) {
     data = userword === word ? 'Valid word' : 'Invalid word';
     console.log(userword.length)
@@ -13,47 +14,107 @@ function checkWord(userword, word) {
         }
     }
 }
-var num = 0
+*/
+function checkWord(){
+
+}
 window.onload = function findNumber(){
     fetch('/word')
         .then(response => response.text())
         .then(data => {
-            num = data.slice(0, -1)
-            console.log(num.length)
+            num = data.slice(0, -1).length
+            word = data.slice(0, -1)
+            tabword = word.split("")
+            console.log(num)
+            console.log(word)
+            console.log(tabword)
         })
 }
-
+var numTry = 0
+var success = 0
 const form = document.getElementById('motForm');
 form.addEventListener('submit', (event) => {
     event.preventDefault();
-    const userWord = document.getElementById('mot').value;
+    if (numTry != 5){
+        incrNum = numTry+1
+    }
+    var letterUser = ""
+    for (i = 0; i<num; i++){
+        console.log("numTry = " + numTry)
+        letterUser = document.getElementById('case'+i+numTry).value;
+        var trueLetter = tabword.at(i)
+        console.log(letterUser + 'case'+i+numTry)
+        if (trueLetter == letterUser){
+            document.getElementById('case'+i+numTry).style.backgroundColor = '#93ff8a';
+            success +=1
+            var input = document.getElementById("case" + i + incrNum);
+            input.placeholder = trueLetter
+        }
+        /*else if(tabword[found]){
+            if (typeof(myVariable) != "undefined"){
+
+            }
+        }*/
+        
+    }
+    if (success == num){
+        console.log("success")
+        addElement()
+    }
+    else{
+        success = 0
+    }
+    
+    numTry += 1
+    console.log("fin de form" + numTry)
+    
+   /*
+    const userWord = document.getElementById('case').value;
     fetch('/word?mot='+userWord)
         .then(response => response.text())
         .then(data => {
             checkWord(userWord, data.slice(0, -1));
-        })
+        })*/
     //    .catch(error => {
     //        console.error('Error:', error);
     //        document.getElementById('result').textContent = "erreur";
     //    });
 });
-
-
+function addElement() {
+    // crée un nouvel élément div
+    var newDiv = document.createElement("div");
+    // et lui donne un peu de contenu
+    var newContent = document.createTextNode("Bravo vous avez trouvé le mot !");
+    // ajoute le nœud texte au nouveau div créé
+    newDiv.appendChild(newContent);
+  
+    // ajoute le nouvel élément créé et son contenu dans le DOM
+    var currentDiv = document.getElementById("bravo");
+    document.body.insertBefore(newDiv, currentDiv);
+  }
+var word = ""
+var num = 0
 function addFields(){
-    // Generate a dynamic number of inputs
-    var number = num.length;
+    var number = num;
     // Get the element where the inputs will be added to
     var container = document.getElementById("container");
     // Remove every children it had before
-    //while (container.hasChildNodes()) {
-    //    container.removeChild(container.lastChild);}
-    for (i=0;i<number;i++){
-        var input = document.createElement("input");
-        input.type = "text";
-        input.name = "member" + i;
-        input.maxLength = "1"
-        container.appendChild(input);
-        // Append a line break 
+    while (container.hasChildNodes()) {
+        container.removeChild(container.lastChild);
     }
+    for (j=0; j<6; j++){
+        for (i=0;i<number;i++){
+            var input = document.createElement("input");
+            input.type = "text";
+            input.id = "case" + i + j
+            input.name = "case" + i +j;
+            input.maxLength = "1"
+            container.appendChild(input);
+            // Append a line break 
+        }
+        container.appendChild(document.createElement("br"));
+        
+    }var input = document.getElementById("case" + i + j+1)
+    
 }
 
