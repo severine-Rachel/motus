81fa56254514aa5057eca41f25e54193bf0b6b0e
severine-Rachel <severine.roux4@gmail.com>
2024-03-15 21:58:31 +0100

    score pb res.send

--- Score/score/app.js
+++ Score/score/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 app.use(express.static('/public'));
 const cors = require('cors');
+app.use(express.json());
 var bodyParser = require('body-parser')
 // Activer CORS avec des options spécifiques pour votre application web
 app.use(cors({
@@ -12,17 +13,37 @@ app.use(cors({
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/public/index.html');
 })
-
+/*
 app.get('/setscore', (req, res) => {
   const score = parseInt(req.query.score);
   console.log(score)
   res.send(`${score}`);
 });
 
+app.post('/setscore', (req, res) => {
+  const score = parseInt(req.query.score);
+  console.log(score)
+  res.send(`${score}`);
+});
+*/
 app.get('/getscore', (req, res) => {
   score2 = req.body.score;
   res.send('score');
 });
 const port = 4000;
 app.listen(port, () => {
+  console.log(`Serveur lancé sur http://localhost:${port}`);
 });
+
+app.get('/setscore',  async (req, res) => {
+  //On récupère le nom du joueur actuel dans l'url
+  let body = req.body;
+  let score = body.score;
+  console.log(score);
+  //Si l'utilisateur n'est pas connecté, on ne renvoie rien
+  if(score==undefined || score=="undefined"){
+    res.send("No score");
+    return;
+  }
+  res.send(score);
+})
